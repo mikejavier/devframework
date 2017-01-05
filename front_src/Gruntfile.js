8445c3ec31cec6990d37261014fd18f012dd7b99
Michael Santillán <contato@michaelsantilln.com>
2017-01-05 13:41:30 -0300

    update configs in gruntfile to create fast bundle

--- front_src/Gruntfile.js
+++ front_src/Gruntfile.js
@@ -2,139 +2,139 @@
 
 module.exports = function( grunt ) {
 
-	// Definição dos arquivos js
-	var filesJS = ['src/js/main.js'];
+    // Definição dos arquivos js
+    var filesJS = ['src/tmp/js/main.js']
 
-	// Definição dos arquivos css
-	var sassconcat = ['node_modules/normalize.css/normalize.css', 'src/tmp/css/sprite/sprite.css', 'src/tmp/css/main_sass.css'];
+    // Definição dos arquivos css
+    var sassconcat = ['node_modules/normalize.css/normalize.css', 'src/tmp/css/sprite/sprite.css', 'src/tmp/css/main_sass.css'];
 
-	// Load all tasks
-	require('time-grunt')(grunt);
-	require('jit-grunt')(grunt,{
-		cmq: 'grunt-combine-media-queries',
-	});
+    // Load all tasks
+    require('time-grunt')(grunt);
+    require('jit-grunt')(grunt,{
+        cmq: 'grunt-combine-media-queries',
+    });
 
-	grunt.initConfig({
-		// Watch
-		watch: {
-			precss: {
-				files: [ 'src/sass/**/*' ],
-				tasks: [ 'sass', 'concat:sass' ]
-			},
+    grunt.initConfig({
+        // Watch
+        watch: {
+            precss: {
+                files: [ 'src/sass/**/*' ],
+                tasks: [ 'sass', 'concat:sass' ]
+            },
 
-			js: {
-				files: 'src/js/**/*',
-				tasks: [ 'browserify']
-			},
+            js: {
+                files: 'src/tmp/js/main.js',
+                tasks: ['concat:js']
+            },
 
-			pug: {
-				files: 'views/**/*',
-				tasks: [ 'pug' ]
-			}
-		},
+            pug: {
+                files: 'views/**/*',
+                tasks: [ 'pug' ]
+            }
+        },
 
-	    sass: {
-	        options: {
-	            sourceMap: true
-	        },
-	        dist: {
-	            files: {
-	                'src/tmp/css/main_sass.css': 'src/css/main.scss'
-	            }
-	        }
-	    },
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'src/tmp/css/main_sass.css': 'src/css/main.scss'
+                }
+            }
+        },
 
-	    pug: {
-	        compile: {
-	            options: {
-	                client: false,
-	                pretty: true
-	            },
-	            // Create a singles pages
-	            files: [ {
-	              cwd: "views/",
-	              src: "**/*.pug",
-	              dest: "../",
-	              expand: true,
-	              ext: ".html"
-	            } ]
-            	/*
-            	= Create a templates =
-	            files: {
-			      "path/to/dest.html": "path/to/templates/*.jade"
-			    }
-			    */
-	        }
-	    },
+        pug: {
+            compile: {
+                options: {
+                    client: false,
+                    pretty: true
+                },
+                // Create a singles pages
+                files: [ {
+                  cwd: "views/",
+                  src: "**/*.pug",
+                  dest: "../",
+                  expand: true,
+                  ext: ".html"
+                } ]
+                /*
+                = Create a templates =
+                files: {
+                  "path/to/dest.html": "path/to/templates/*.jade"
+                }
+                */
+            }
+        },
 
-		// Concateção dos arquivos CSS e JS
-		concat: {
-			sass: {
-				src: sassconcat,
+        // Concateção dos arquivos CSS e JS
+        concat: {
+            sass: {
+                src: sassconcat,
 
-				dest: '../dist/css/styles.combined.min.css'
-			},
+                dest: '../dist/css/styles.combined.min.css'
+            },
 
-			js: {
-				src: filesJS,
+            js: {
+                src: filesJS,
 
-				dest: '../dist/js/scripts.combined.min.js'
-			}
-		},
+                dest: '../dist/js/scripts.combined.min.js'
+            }
+        },
 
-		// Concateção e minificação dos arquivos e JS
-		uglify: {
-			options: {
-				mangle: false
-			},
+        // Concateção e minificação dos arquivos e JS
+        uglify: {
+            options: {
+                mangle: false
+            },
 
-			dist: {
-				files: {
-					'../dist/js/scripts.combined.min.js': '../dist/js/scripts.combined.min.js'
-				}
-			}
-		},
+            dist: {
+                files: {
+                    '../dist/js/scripts.combined.min.js': '../dist/js/scripts.combined.min.js'
+                }
+            }
+        },
 
-		cssmin: {
-		  target: {
-		    files: [{
-		      expand: true,
-		      cwd: '../dist/css',
-		      src: ['*.css'],
-		      dest: '../dist/css',
-		      ext: '.combined.min.css'
-		    }]
-		  }
-		},
+        cssmin: {
+          target: {
+            files: [{
+              expand: true,
+              cwd: '../dist/css',
+              src: ['*.css'],
+              dest: '../dist/css',
+              ext: '.combined.min.css'
+            }]
+          }
+        },
 
-		// Otimização das imagens
-		imagemin: {
-		    dynamic: {
-		    	files: [{
-		        	expand: true,
-		        	cwd: 'src/images',
-		        	src: ['**/*.{png,jpg,gif}'],
-		        	dest: '../dist/images'
-		    	}]
-		    }
-		},
+        // Otimização das imagens
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/images',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: '../dist/images'
+                }]
+            }
+        },
 
-		cmq: {
-			target:{
-		      files: {
-		        '../dist/css': ['../dist/css/*.css']
-		      }
-		    }
-	    },
+        cmq: {
+            target:{
+              files: {
+                '../dist/css': ['../dist/css/*.css']
+              }
+            }
+        },
 
-		browserSync: {
+        browserSync: {
             files: {
 
                 // Aplicando o recurso de Live Reload nos seguintes arquivos
                 src : [
-                	'../dist/css/styles.combined.min.css',
-                	'../**/*.html',
-                	'../**/*.php',
+                    '../dist/css/styles.combined.min.css',
+                    '../**/*.html',
+                    '../**/*.php',
                 ]
 
             },
@@ -148,7 +148,7 @@ module.exports = function( grunt ) {
 
                 // Sincronizando os eventos entre os dispositívos
                 ghostMode: {
-                	clicks: true,
+                    clicks: true,
                     scroll: true,
                     links: true,
                     forms: true
@@ -156,53 +156,58 @@ module.exports = function( grunt ) {
             }
         },
 
-		sprity: {
-			options: {
-			'cssPath': '../images',
-			'processor': 'css',
-			'orientation': 'vertical',
-			'margin': 4,
-			'prefix':'sprite'
-			},
-			sprite: {
-				options: {
-				  'style': '../tmp/css/sprite/sprite.css'
-				},
-				src: 'src/images/sprite/*',
-				dest: 'src/images/sprite',
-			}
-	    },
+        sprity: {
+            options: {
+            'cssPath': '../images',
+            'processor': 'css',
+            'orientation': 'vertical',
+            'margin': 4,
+            'prefix':'sprite'
+            },
+            sprite: {
+                options: {
+                  'style': '../tmp/css/sprite/sprite.css'
+                },
+                src: 'src/images/sprite/*',
+                dest: 'src/images/sprite',
+            }
+        },
 
         devUpdate: {
-	        main: {
-	            options: {
-	                updateType: 'force', //just report outdated packages
-	                reportUpdated: false, //don't report up-to-date packages
-	                semver: false, //stay within semver when updating
-	                packages: {
-	                    devDependencies: true, //only check for devDependencies
-	                    dependencies: false
-	                },
-	                packageJson: null, //use matchdep default findup to locate package.json
-	                reportOnlyPkgs: [] //use updateType action on all packages
-	            }
-	        }
-	    },
+            main: {
+                options: {
+                    updateType: 'force', //just report outdated packages
+                    reportUpdated: false, //don't report up-to-date packages
+                    semver: false, //stay within semver when updating
+                    packages: {
+                        devDependencies: true, //only check for devDependencies
+                        dependencies: false
+                    },
+                    packageJson: null, //use matchdep default findup to locate package.json
+                    reportOnlyPkgs: [] //use updateType action on all packages
+                }
+            }
+        },
         browserify: {
             dist: {
                 files: {
-                  'src/js/main.js': filesJS
+                  'src/tmp/js/main.js': 'src/js/main.js'
                 },
                 options: {
-                     'transform': [ ['babelify', { 'presets': ['es2015'] }] ]
+                    transform: [ ['babelify', { 'presets': ['es2015'] }] ],
+                    watch : true, // use watchify for incremental builds!
+                    //keepAlive : true, // watchify will exit unless task is kept alive
+                    browserifyOptions : {
+                        debug : true // source mapping
+                    }
                 }
             }
         }
-	});
+    });
 
-	// registrando tarefa default
-	grunt.registerTask( 'default', [ 'browserSync', 'watch' ] );
-	grunt.registerTask( 'dist', [ 'uglify:dist', 'cmq', 'cssmin', 'imagemin' ] );
-	grunt.registerTask( 'img', [ 'sprity', 'imagemin' ] );
-	grunt.registerTask( 'update', [ 'devUpdate' ] );
+    // registrando tarefa default
+    grunt.registerTask( 'default', [ 'browserSync', 'browserify', 'watch' ] )
+    grunt.registerTask( 'dist', [ 'uglify:dist', 'cmq', 'cssmin', 'imagemin' ] );
+    grunt.registerTask( 'img', [ 'sprity', 'imagemin' ] );
+    grunt.registerTask( 'update', [ 'devUpdate' ] );
 };
